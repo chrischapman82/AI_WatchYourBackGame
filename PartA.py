ddf0188ed9f64bf5390bb73eaff08edaaf4f90c6
Christopher Chapman <christopher.chapman8296@gmail.com>
2018-03-23 22:57:21 +1100

    Added classes for Coordinates

--- PartA.py
+++ PartA.py
@@ -1,4 +1,5 @@
 from queue import *
+import heapq
 
 #sample input
 a ="X - - - - - - X - - - - - - - - - - - - - 0 0 - - - - - @ 0 - - - - - - - - - - - - - - - 0 - - - - - - @ - @ @ X - - - - - - X"
@@ -18,6 +19,21 @@ LEFT = 3
 NOT_VISITED = -1
 BOARD_LEN = 8
 
+# Class for coordinates because they're easier like this
+class Coordinate:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):  # For printing
+        return "[" + str(self.x) + "," + str(self.y) + "]"
+
+    def __repr__(self):
+        return "[" + str(self.x) + "," + str(self.y) + "]"
+
+#class Node:
+    #def __init__(self,):
+        #self.f = f
 
 ############## MOVES ################
 def countMovesByColour(colour, board):
@@ -26,50 +42,54 @@ def countMovesByColour(colour, board):
     for x in range(BOARD_LEN):
         print(board[x])
         for y in range(BOARD_LEN):
-            
+
             if board[x][y] == colour:
-                count += len(getAdjacentMoves(x, y, board))
+                curr_coord = Coordinate(x,y)    #todo use this for getAdj
+                count += len(getAdjacentMoves(curr_coord, board))
     print(count)
 
-
-        
 # Changed so this now returns an array of the adjacent moves
 # in form [(x,y),(x2,y2)...]
 # Better for part 2 and easy to get count from len!
-def getAdjacentMoves(x, y, board):
+def getAdjacentMoves(coord, board):
     count = 0
     moves = []
     for dir in DIRECTIONS:
-        curr_x, curr_y = getCoordInDir(x,y, dir)
+        curr = getCoordInDir(coord, dir)
 
         #checks if out of bounds!
-        if isOutOfBounds(curr_x,curr_y,board):
+
+        if isOutOfBounds(curr,board):
             #Can't move here
             continue
-        
-        curr_piece = board[curr_x][curr_y]
+
+        curr_piece = board[curr.x][curr.y]  #todo
 
         #if a piece blocks its way, checks if it can jump over
         #this changes curr_x and curr_y to the jump ones
         if (curr_piece == WHITE_PIECE or curr_piece == BLACK_PIECE):
-            curr_x, curr_y = getCoordInDir(curr_x, curr_y, dir)
-            if (isOutOfBounds(curr_x, curr_y, board) or
-                board[curr_x][curr_y] != EMPTY):
+            curr = getCoordInDir(curr, dir)     #todo
+            if (isOutOfBounds(curr, board) or
+                board[curr.x][curr.y] != EMPTY):
                 #Can't jump over piece
                 continue
-            
-        moves += [(curr_x,curr_y)]
-    return moves             
 
+        moves += [curr]  #todo
+    return moves
 
-def isOutOfBounds(x,y,board):
-    if (x<0 or x>7 or y<0 or y>7 or board[x][y] == CORNER):
+def isOutOfBounds(coord, board):   #todo
+    x = coord.x
+    y = coord.y
+    if x<0 or x>7 or y<0 or y>7 or board[x][y] == CORNER:   #todo use static variables
         return True
     return False
 
 #gets the coordinate in the given direction
-def getCoordInDir(x,y, dir):
- 
+def getCoordInDir(coord, dir):
+
+    x = coord.x
+    y = coord.y
+
     if dir == LEFT:
         x = x-1
     elif dir == UP:
@@ -79,7 +99,7 @@ def getCoordInDir(x,y, dir):
     elif dir == DOWN:
         y = y-1
 
-    return x,y
+    return Coordinate(x,y)
 
 
 ############## MASSACRE ################
@@ -89,72 +109,73 @@ def getCoordInDir(x,y, dir):
 #Could get pairs of kill spots!
     #killable if 2 opposite spaces are open
     #For each kill pair:
-        #
 #While Black array is !empty -> keep running
 
 def killThemAll(board):
-    
+
     killable = []
-    
+
     for x in range(BOARD_LEN):
         for y in range(BOARD_LEN):
             if board[x][y] == BLACK_PIECE:
                 print("Checking ")
                 print(x,y)
+
+                coord = Coordinate(x,y)
                 # Checks for kill pairs. Then, adds them to array
-                killable += (isKillable(WHITE_PIECE, x, y, board))
-                print(killable)
+                killable += (isKillable(WHITE_PIECE, coord.x, coord.y, board))
+                for i in range(len(killable)):
+                    findClosestPiece(killable[i][0])
+                    findClosestPiece(killable[i][1])
 
-                findClosestPiece(killable[0][0])
-
+#returns the minigun distance
+def getManhattanDistance(start, finish):
+    return abs(start.x-finish.x) + abs(start.y-finish.y)
 
 # Checks if a peice is killable and returns the possible kill pairs as
 # as an array of tuples
 def isKillable(colour, x, y, board):
-    killable = []
+    killable = []   #todo change signature to coord
 
     # Checks horizontal pair
-    if (isKillSpot(getCoordInDir(x,y,LEFT), board, colour) and
-        isKillSpot(getCoordInDir(x,y,RIGHT), board, colour)):
+
+    coord = Coordinate(x,y)
+    if (isKillSpot(getCoordInDir(coord,LEFT), board, colour) and
+        isKillSpot(getCoordInDir(coord,RIGHT), board, colour)):
 
         #this means killable = [[(2,4),(4,4)],[...]]
         #puts the array in the form [PAIR,PAIR,PAIR]...
         #first pair is accessed through k[0]
         #side of pair is accesssed through k[0][0]
         #x of side of pair is accessed through k[0][0][0]
-        killable += [[(getCoordInDir(x,y,LEFT)), getCoordInDir(x,y,RIGHT)]]
-                
+        killable += [[(getCoordInDir(coord,LEFT)), getCoordInDir(coord,RIGHT)]]
+
     # checks vertical pair
-    if (isKillSpot(getCoordInDir(x,y,UP), board, colour) and
-        isKillSpot(getCoordInDir(x,y,DOWN), board, colour)):
-        killable += [[(getCoordInDir(x,y,UP)), (getCoordInDir(x,y,DOWN))]]
+    if (isKillSpot(getCoordInDir(coord,UP), board, colour) and
+        isKillSpot(getCoordInDir(coord,DOWN), board, colour)):
+        killable += [[(getCoordInDir(coord,UP)), (getCoordInDir(coord,DOWN))]]
     #print(killable)
     return killable
 
-        
-
 def isKillSpot(coord, board, colour):
-
-    if isOffBoard(coord, board):
+    if isOffBoard((coord.x, coord.y), board):
         return False
-    
-    curr = board[coord[0]][coord[1]]
+
+    curr = getPieceFromCoord(coord, board)
     return (curr == CORNER or
             curr == colour or
             curr == EMPTY)
 
-#def findClosestPair():
-    #TODO
-
+def getPieceFromCoord(coord, board):
+    return board[coord.x][coord.y]
 
 #finds the closest piece
 #loc is a tuple (x,y)
 #Basically BFS
 
 #currently get adjacent isn't checking for our pieces
-#If we use the closer of the 2 
-def findClosestPiece(loc):
-
+#If we use the closer of the 2
+def findClosestPiece(coord):
     #init the visited array to NOT visisted
     visited = []
     for i in range(BOARD_LEN):
@@ -162,23 +183,24 @@ def findClosestPiece(loc):
         for j in range(BOARD_LEN):
             temp_line.append(NOT_VISITED)
         visited.append(temp_line)
-        
+
     dist = 0
-    visited[loc[0]][loc[1]] = dist
+    visited[coord.x][coord.y] = dist
 
     #init the queue
     q = Queue()
-    q.put(loc)
-    
+    q.put(coord)
+
     while not q.empty(): #TODO
         dist += 1
         curr = q.get()
-        adj = getAdjacentMoves(curr[0],curr[1],board)
+        adj = getAdjacentMoves(curr,board) #todo
         for i in range(len(adj)):
             next = adj[i]
-            if visited[adj[i][0]][adj[i][1]] == NOT_VISITED:
+            if visited[next.x][next.y] == NOT_VISITED:
                 q.put(next)
-                visited[adj[i][0]][adj[i][1]] = visited[curr[0]][curr[1]]+1
+                visited[next.x][next.y] = visited[curr.x][curr.y]+1
+                #visited[adj[i][0]][adj[i][1]] = visited[curr[0]][curr[1]]+1
 
     printBoard(visited)
 
@@ -189,15 +211,25 @@ def printBoard(board):
         print("|",end="")
         for j in range(BOARD_LEN):
             print("{0:3d}".format(board[i][j]),end="")
-        print("|")    
-                  
+        print("|")
 
-def isOffBoard(coord,board):
-    x = coord[0]
-    y = coord[1]
-    if (x<0 or x>7 or y<0 or y>7):
+
+def isOffBoard(coord,board):    #todo cahnge to otehr
+    coord = Coordinate(coord[0],coord[1])
+    x = coord.x
+    y = coord.y
+    if (x<0 or x>7 or y<0 or y>7 or board[x][y] == CORNER):
         return True
     return False
+
+# Using Heap queues from: https://dbader.org/blog/priority-queues-in-python
+def a_star(start, finish):
+    Q = []
+
+    heapq.heappush(Q,(4,start))
+    heapq.heappush(Q, (3, finish))
+    print(Q)
+
 
 
 ############## START PROGRAM ################
@@ -221,9 +253,11 @@ print(board)
 # count Moves by Colour
 
 #if (input() = "Moves"):
-    
+
 
 countMovesByColour(BLACK_PIECE, board)
 countMovesByColour(WHITE_PIECE, board)
 
 killThemAll(board)
+
+a_star(Coordinate(3,3), Coordinate(5,5))
